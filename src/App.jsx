bbe21d21eac1e2ee0b556a33a2e80ef4d842a6bb
SomaSekharMarella <2300030411@kluniversity.in>
2025-10-06 16:47:35 +0530

    added all the files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project</h1>
+      <h1>M Soma Sekhar</h1>
+      <h1>CICD DevOps</h1>
+      <h1>S105</h1>
+      <p>Okayy byee lets see</p>
+    </div>
+  );
+}
+
+export default App;
